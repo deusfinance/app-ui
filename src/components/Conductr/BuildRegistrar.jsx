91a8c8e8d83c32b8f1e1bf7c4b4f3adbd92dcd9b
Mohammad Ali <maliabr75@gmail.com>
2020-10-24 03:45:02 +0330

    added new page to conductr

--- src/components/Conductr/BuildRegistrar.jsx
+++ src/components/Conductr/BuildRegistrar.jsx
@@ -2,8 +2,72 @@ import React, { Component } from 'react';
 import './steps.scss'
 
 class BuildRegistrar extends Component {
-    state = {}
+    state = {
+        query: "",
+        isFocus: false,
+        assets: [
+            {
+                name: "tsla",
+                real_name: "U.S.: NASDAQ",
+                inc: "TESLA Inc.",
+                price: "448.16",
+                new_price: "468.18",
+            },
+            {
+                name: "tsla inverted ",
+                real_name: "U.S.: NASDAQ",
+                inc: "TESLA Inc.",
+                price: "230.16",
+                new_price: "220.73",
+            },
+            {
+                name: "qqq",
+                real_name: "U.S.: NASDAQ",
+                inc: "TESLA Inc.",
+                price: "100.92",
+                new_price: "100.92",
+            },
+            {
+                name: "qqq inverted",
+                real_name: "U.S.: NASDAQ",
+                inc: "TESLA Inc.",
+                price: "120.92",
+                new_price: "145.37",
+            },
+        ]
+    }
+
+    computeProfit = (prevPrice, newPrice) => {
+
+        if (prevPrice === newPrice) {
+            return { price: 0, percentage: 0, className: "profit-zero" }
+        } else if (prevPrice > newPrice) {
+
+            return { price: (newPrice - prevPrice).toFixed(2), percentage: (-1 * ((prevPrice - newPrice) / prevPrice) * 100).toFixed(2), className: "profit-down" }
+
+        } else {
+            return { price: '+' + (newPrice - prevPrice).toFixed(2), percentage: '+' + (((newPrice - prevPrice) / prevPrice) * 100).toFixed(2), className: "profit-up" }
+        }
+    }
+
+    handleSearchInputChange = (query) => {
+        this.setState({ query: query })
+    }
+    onBlur = () => {
+        this.setState({ isFocus: false })
+    }
+    onFocus = () => {
+        this.setState({ isFocus: true })
+    }
+
     render() {
+        const { query, assets, isFocus } = this.state
+        const filterdAssets = assets.filter(s => {
+            if (s.real_name.includes(query) || s.name.includes(query)) {
+                return true
+            }
+            return false
+        })
         return (<>
 
             <div className="conductr-wrap">
@@ -23,7 +87,7 @@ class BuildRegistrar extends Component {
                                 <div className="step-number">step 1</div>
                                 <div className="step-title">Build your registrar</div>
                                 <div className="step-info">
-                                    <pre>{`
+                                    {!isFocus && <pre>{`
 Use the search bar to find desired assets that you want to mirror.
 
 You can select currently two different assets.
@@ -38,7 +102,13 @@ In the v0.1 Pilot we are offering
 
 500 + more will be added in december
                                     `}
-                                    </pre>
+                                    </pre>}
+                                    {isFocus && <pre>{`
+Use the search bar to find desired assets that you want to mirror to conduct your personalised registrar. 
+
+You can select as many different assets as you wish and change their allocation.
+                                    `}
+                                    </pre>}
                                 </div>
                                 <div className="bar-wrap">
                                     <div className="bar-chart-wrap">
@@ -53,7 +123,66 @@ In the v0.1 Pilot we are offering
                                     </div>
                                 </div>
                             </div>
-                            <div className="tools"></div>
+                            <div className="tools">
+                                <div className="pilot-wrap-step1">
+                                    <div className="pilot">
+                                        <div className="title">
+                                            conduct <img src={`${process.env.PUBLIC_URL}/img/conducr-icon.svg`} alt="" />
+                                        </div>
+                                        <div className="title-pilot">pilot</div>
+                                    </div>
+                                </div>
+
+                                <div className="note"> Note: for pilot puroses we’re only supporting tsla +/- and qqqq +/-
+
+                        </div>
+
+                                <div className="symbol-wrap">
+                                    <input
+                                        className="symbol"
+                                        type="text"
+                                        id="symbol"
+                                        placeholder="Enter a symbol or keyword"
+                                        onChange={(e) => this.handleSearchInputChange(e.currentTarget.value)}
+                                        onFocus={this.onFocus} onBlur={this.onBlur}
+                                    />
+                                </div>
+
+                                {isFocus && <div className="search-wrap">
+                                    <div className="search-output">
+                                        {
+                                            filterdAssets.map(s => {
+                                                let profit = this.computeProfit(s.price, s.new_price)
+                                                return <>
+                                                    <div className="search-item">
+                                                        <div className="search-left">
+                                                            <div className="search-top">
+                                                                <div className="name">
+                                                                    <span>{s.name}</span> <span>{s.real_name}</span>
+                                                                </div>
+
+                                                                <div className="price">{s.new_price}</div>
+                                                            </div>
+                                                            <div className="search-bottom">
+                                                                <div className="p1">{s.inc}</div>
+                                                                <div className={profit.className}>{profit.price} ({profit.percentage}%)</div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="search-right">
+                                                            <div className="add-btn-wrap">
+                                                                <div className="add-btn">Add</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            })
+                                        }
+                                    </div>
+                                </div>}
+
+
+
+                            </div>
                         </div>
                     </div>
 

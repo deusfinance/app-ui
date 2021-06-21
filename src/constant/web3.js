da375c0f194ec2fca43a6dc192cbfff2122e9fc9
Mohammad Ali <maliabr75@gmail.com>
2021-06-21 17:13:32 +0430

    Clean chains config code

--- src/constant/web3.js
+++ src/constant/web3.js
@@ -27,3 +27,87 @@ export const NameChainMap = {
     97: "BSCT",
     137: "POLYGON",
 }
+
+export const NetworksData = {
+    [ChainMap.MAINNET]: {
+        chainId: "0x1",
+        chainName: "Ethereum Mainnet",
+        nativeCurrency: {
+            name: "ETH",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: ["https://mainnet.infura.io/v3/undefined"],
+        blockExplorerUrls: ["https://etherscan.io/"],
+        iconUrls: []
+
+    },
+    [ChainMap.XDAI]: {
+        chainId: "0x64",
+        chainName: "xDAI Chain",
+        nativeCurrency: {
+            name: "xDAI",
+            symbol: "xDAI",
+            decimals: 18,
+        },
+        rpcUrls: ["https://rpc.xdaichain.com/"],
+        blockExplorerUrls: ["https://blockscout.com/poa/xdai/"],
+        iconUrls: ["https://app.deus.finance/tokens/xdai.svg"]
+    },
+    [ChainMap.MATIC]: {
+        chainId: "0x89",
+        chainName: "Matic Mainnet",
+        nativeCurrency: {
+            name: "Matic",
+            symbol: "MATIC",
+            decimals: 18,
+        },
+        rpcUrls: ["https://rpc-mainnet.matic.network", "https://rpc-mainnet.maticvigil.com", "https://rpc-mainnet.matic.quiknode.pro", "https://matic-mainnet.chainstacklabs.com", "https://matic-mainnet-full-rpc.bwarelabs.com", "https://matic-mainnet-archive-rpc.bwarelabs.com"],
+        blockExplorerUrls: ["https://explorer.matic.network/"],
+        iconUrls: []
+    },
+    [ChainMap.BSC]: {
+        chainId: "0x38",
+        chainName: "Binance Smart Chain Mainnet",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18,
+        },
+        rpcUrls: ["https://bsc-dataseed1.binance.org"],
+        blockExplorerUrls: ["https://bscscan.com"],
+    },
+    [[ChainMap.BSC]]: {
+        chainId: "0x61",
+        chainName: "Binance Smart Chain Testnet",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18,
+        },
+        rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
+        blockExplorerUrls: ["https://testnet.bscscan.com"],
+    },
+    [ChainMap.HECO]: {
+        chainId: "0x80",
+        chainName: "Huobi ECO Chain Mainnet",
+        nativeCurrency: {
+            name: "HT",
+            symbol: "HT",
+            decimals: 18,
+        },
+        rpcUrls: ["https://http-mainnet.hecochain.com"],
+        blockExplorerUrls: ["https://hecoinfo.com"],
+    },
+    [ChainMap.HECO_TESTNET]: {
+        chainId: "0x100",
+        chainName: "Huobi ECO Chain Testnet",
+        nativeCurrency: {
+            name: "htt",
+            symbol: "htt",
+            decimals: 18,
+        },
+        rpcUrls: ["https://http-testnet.hecochain.com"],
+        blockExplorerUrls: ["https://testnet.hecoinfo.com"],
+    },
+}
--- src/services/addRPC.js
+++ src/services/addRPC.js
@@ -1,4 +1,4 @@
-import { ChainMap } from '../constant/web3';
+import { ChainMap, NetworksData } from '../constant/web3';
 import { ToastTransaction } from '../utils/explorers';
 
 export const addRPC = (account, activate, chainId = 100) => {
@@ -20,88 +20,4 @@ export const addRPC = (account, activate, chainId = 100) => {
                 console.log('We can encrypt anything without the key.');
             });
     }
-}
-
-const NetworksData = {
-    1: {
-        chainId: "0x1",
-        chainName: "Ethereum Mainnet",
-        nativeCurrency: {
-            name: "ETH",
-            symbol: "ETH",
-            decimals: 18,
-        },
-        rpcUrls: ["https://mainnet.infura.io/v3/undefined"],
-        blockExplorerUrls: ["https://etherscan.io/"],
-        iconUrls: []
-
-    },
-    100: {
-        chainId: "0x64",
-        chainName: "xDAI Chain",
-        nativeCurrency: {
-            name: "xDAI",
-            symbol: "xDAI",
-            decimals: 18,
-        },
-        rpcUrls: ["https://rpc.xdaichain.com/"],
-        blockExplorerUrls: ["https://blockscout.com/poa/xdai/"],
-        iconUrls: ["https://app.deus.finance/tokens/xdai.svg"]
-    },
-    137: {
-        chainId: "0x89",
-        chainName: "Matic Mainnet",
-        nativeCurrency: {
-            name: "Matic",
-            symbol: "MATIC",
-            decimals: 18,
-        },
-        rpcUrls: ["https://rpc-mainnet.matic.network", "wss://ws-mainnet.matic.network"],
-        blockExplorerUrls: ["https://explorer.matic.network/"],
-        iconUrls: []
-    },
-    56: {
-        chainId: "0x38",
-        chainName: "Binance Smart Chain Mainnet",
-        nativeCurrency: {
-            name: "BNB",
-            symbol: "BNB",
-            decimals: 18,
-        },
-        rpcUrls: ["https://bsc-dataseed1.binance.org"],
-        blockExplorerUrls: ["https://bscscan.com"],
-    },
-    97: {
-        chainId: "0x61",
-        chainName: "Binance Smart Chain Testnet",
-        nativeCurrency: {
-            name: "BNB",
-            symbol: "BNB",
-            decimals: 18,
-        },
-        rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
-        blockExplorerUrls: ["https://testnet.bscscan.com"],
-    },
-    128: {
-        chainId: "0x80",
-        chainName: "Huobi ECO Chain Mainnet",
-        nativeCurrency: {
-            name: "HT",
-            symbol: "HT",
-            decimals: 18,
-        },
-        rpcUrls: ["https://http-mainnet.hecochain.com"],
-        blockExplorerUrls: ["https://hecoinfo.com"],
-    },
-    256: {
-        chainId: "0x100",
-        chainName: "Huobi ECO Chain Testnet",
-        nativeCurrency: {
-            name: "htt",
-            symbol: "htt",
-            decimals: 18,
-        },
-        rpcUrls: ["https://http-testnet.hecochain.com"],
-        blockExplorerUrls: ["https://testnet.hecoinfo.com"],
-    },
 }

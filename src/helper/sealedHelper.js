0cf1feecaa296b395a2088d1c3d47c2d97414c14
Mohammad Ali <maliabr75@gmail.com>
2021-06-23 18:27:50 +0430

    Update sealedHelper

--- src/helper/sealedHelper.js
+++ src/helper/sealedHelper.js
@@ -3,10 +3,10 @@
 // import { getSealedSwapperContract} from "./contractHelpers"
 import { TransactionState } from "../utils/constant"
 import { SwapTranaction } from "../utils/explorers"
-import { getToWei } from "./formatBalance"
+import { fromWei, getToWei } from "./formatBalance"
 import { isZero } from "../constant/number"
 import {
-	// getSealedSwapperContract,
+	getSealedSwapperContract,
 	getUniswapV2Contract,
 	getUniswapRouterContract,
 	getDeusAutomaticMarketMakerContract,
@@ -17,7 +17,9 @@ import addresses from '../constant/addresses.json';
 import BigNumber from "bignumber.js"
 import multicall from "./multicall"
 
-let getSealedSwapperContract = () => { }
+
+const sealedAddress = "0x09aa2eb99b93579c0467a402bc4b8b3c1fb85f69"
+
 
 export const getSealedAmountsOut = async (fromCurrency, amountIn, chainId, web3) => {
 	const amountInWei = getToWei(amountIn, fromCurrency.decimals).toFixed(0)
@@ -39,14 +41,14 @@ export const getSealedAmountsOut = async (fromCurrency, amountIn, chainId, web3)
 
 
 
-export const sealedSwap = async (fromCurrency, toCurrency, amountIn, amountOut, minAmountOut, account, chainId, web3) => {
+export const swap = async (fromCurrency, toCurrency, amountIn, amountOut, minAmountOut, account, chainId, web3) => {
 	let hash = null
 	const amountInWei = getToWei(amountIn, fromCurrency.decimals).toFixed(0)
 	const minAmountOutWei = getToWei(minAmountOut, toCurrency.decimals).toFixed(0)
 
 	if (amountIn === "" || isZero(amountInWei) || amountOut === "" || isZero(minAmountOutWei)) return { status: false }
-
-	const swapFunc = swapFuncMaker(fromCurrency, amountInWei, minAmountOutWei, chainId, web3)
+	let sealedContract = getSealedSwapperContract(sealedAddress, web3)
+	const swapFunc = swapFuncMaker(fromCurrency, amountInWei, minAmountOutWei, sealedContract, 0)
 	let sendArgs = { from: account }
 
 	return swapFunc
@@ -75,74 +77,53 @@ export const sealedSwap = async (fromCurrency, toCurrency, amountIn, amountOut, 
 }
 
 
-export const swapFuncMaker = async (fromCurrency, amountInWei, minAmountOutWei, chainId, web3, balancerMinAmountsOut) => {
+export const swapFuncMaker = (fromCurrency, amountInWei, minAmountOutWei, sealedContract, balancerMinAmountsOut) => {
 
 	if (fromCurrency.symbol === "BPT") {
-		return bpt2sdea(amountInWei, balancerMinAmountsOut, minAmountOutWei, web3, chainId)
+		return bpt2sdea(amountInWei, balancerMinAmountsOut, minAmountOutWei, sealedContract)
 	}
 	else if (fromCurrency.symbol === "sUniDD") {
-		return sUniDD2sdea(amountInWei, minAmountOutWei, web3, chainId)
+		return sUniDD2sdea(amountInWei, minAmountOutWei, sealedContract)
 	}
 	else if (fromCurrency.symbol === "sUniDE") {
-		return sUniDE2sdea(amountInWei, minAmountOutWei, web3, chainId)
+		return sUniDE2sdea(amountInWei, minAmountOutWei, sealedContract)
 	}
 	else if (fromCurrency.symbol === "sUniDU") {
-		return sUniDU2sdea(amountInWei, minAmountOutWei, web3, chainId)
+		return sUniDU2sdea(amountInWei, minAmountOutWei, sealedContract)
 	}
 }
 
-
-export const bpt2sdea = async (
-	poolAmountIn,
-	balancerMinAmountsOut,
-	minAmountOut,
-	web3, chainId
-) => {
+export const bpt2sdea = async (poolAmountIn, balancerMinAmountsOut, minAmountOut, sealedContract) => {
 	try {
-		await getSealedSwapperContract(web3, chainId)
+		await sealedContract
 			.methods
 			.bpt2sdea(
 				poolAmountIn,
 				balancerMinAmountsOut,
 				minAmountOut
 			)
-			.call()
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-export const sUniDD2sdea = async (sUniDDAmount, minAmountOut, web3, chainId) => {
-	try {
-		await getSealedSwapperContract(web3, chainId)
-			.methods
-			.sUniDD2sdea(sUniDDAmount, minAmountOut)
-			.call()
-	} catch (error) {
-		console.log(error);
-	}
+export const sUniDD2sdea = (sUniDDAmount, minAmountOut, sealedContract) => {
+	return sealedContract
+		.methods
+		.sUniDD2sdea(sUniDDAmount, minAmountOut)
 }
 
-export const sUniDU2sdea = async (sUniDUAmount, minAmountOut, web3, chainId) => {
-	try {
-		await getSealedSwapperContract(web3, chainId)
-			.methods
-			.sUniDU2sdea(sUniDUAmount, minAmountOut)
-			.call()
-	} catch (error) {
-		console.log(error);
-	}
+export const sUniDU2sdea = (sUniDUAmount, minAmountOut, sealedContract) => {
+	console.log(sUniDUAmount.toString(), minAmountOut.toString());
+	return sealedContract
+		.methods
+		.sUniDU2sdea(sUniDUAmount, minAmountOut)
 }
 
-export const sUniDE2sdea = async (sUniDEAmount, minAmountOut, web3, chainId) => {
-	try {
-		await getSealedSwapperContract(web3, chainId)
-			.methods
-			.sUniDE2sdea(sUniDEAmount, minAmountOut)
-			.call()
-	} catch (error) {
-		console.log(error);
-	}
+export const sUniDE2sdea = (sUniDEAmount, minAmountOut, sealedContract) => {
+	return sealedContract
+		.methods
+		.sUniDE2sdea(sUniDEAmount, minAmountOut)
 }
 
 
@@ -168,8 +149,8 @@ export const sUniDDOutGivenIn = async (sUniDDAmount, web3, chainId) => {
 	const contract = getUniswapV2Contract(addresses.token.deus_dea["1"], web3);
 	let totalSupply = await contract.methods.totalSupply().call();
 
-	// let ratio = getSealedSwapperContract(web3, chainId).methods.DDRatio().call() / 1e18;
-	let ratio = 0.9
+	let ratio = await getSealedSwapperContract(sealedAddress, web3).methods.DDRatio().call()
+	ratio = fromWei(ratio, 18)
 
 	const { "0": deusReserve, "1": deaReserve } = await contract.methods.getReserves().call();
 	const [deusMinAmountOut, deaMinAmountOut] = minAmountsCalculator(new BigNumber(sUniDDAmount).times(ratio).toFixed(0), totalSupply, deusReserve, deaReserve)
@@ -182,14 +163,13 @@ export const sUniDDOutGivenIn = async (sUniDDAmount, web3, chainId) => {
 export const sUniDUOutGivenIn = async (sUniDUAmount, web3, chainId) => {
 	const contract = getUniswapV2Contract(addresses.token.dea_usdc["1"], web3);
 	let totalSupply = await contract.methods.totalSupply().call();
-	// let ratio = getSealedSwapperContract(web3, chainId).methods.DURatio().call() / 1e18;
-	let ratio = 0.9
 
-	// let deaReserve, usdcReserve, deaMinAmountOut, usdcMinAmountOut;
+	let ratio = await getSealedSwapperContract(sealedAddress, web3).methods.DURatio().call()
+	ratio = fromWei(ratio, 18)
 
 	const { "0": deaReserve, "1": usdcReserve } = await contract.methods.getReserves().call();
 
-	const [deaMinAmountOut, usdcMinAmountOut] = minAmountsCalculator(new BigNumber(sUniDUAmount).div(100000).times(ratio).toFixed(0), totalSupply, deaReserve, usdcReserve)
+	const [deaMinAmountOut, usdcMinAmountOut] = minAmountsCalculator(new BigNumber(sUniDUAmount).div(100000).times(ratio), totalSupply, deaReserve, usdcReserve)
 
 	const usdc2wethPath = ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"];
 	const ethAmount = await getUniswapRouterContract(web3, chainId).methods.getAmountsOut(usdcMinAmountOut, usdc2wethPath).call()
@@ -206,11 +186,13 @@ export const sUniDEOutGivenIn = async (sUniDEAmount, web3, chainId) => {
 	const contract = getUniswapV2Contract(addresses.token.deus_eth["1"], web3);
 	let totalSupply = await contract.methods.totalSupply().call();
 	// let ratio = getSealedSwapperContract(web3, chainId).methods.DERatio().call() / 1e18;
-	let ratio = 0.9
+
+	let ratio = await getSealedSwapperContract(sealedAddress, web3).methods.DERatio().call()
+	ratio = fromWei(ratio, 18)
 
 	const { "0": deusReserve, "1": wethReserve } = await contract.methods.getReserves().call();
 
-	const [deusMinAmountOut, wethMinAmountOut] = minAmountsCalculator(new BigNumber(sUniDEAmount).times(ratio).toFixed(0), totalSupply, deusReserve, wethReserve)
+	const [deusMinAmountOut, wethMinAmountOut] = minAmountsCalculator(new BigNumber(sUniDEAmount).times(ratio), totalSupply, deusReserve, wethReserve)
 
 	const deusAmount = await calculatePurchaseReturn(wethMinAmountOut, web3, chainId)
 

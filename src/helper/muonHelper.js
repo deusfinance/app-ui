4e1fa3d4d07c07d1352c684f1a3d3d07889e4610
Mohammad Ali <maliabr75@gmail.com>
2021-06-28 22:14:45 +0430

    Add muon functions

--- src/helper/muonHelper.js
+++ src/helper/muonHelper.js
@@ -14,3 +14,41 @@ export const getAmoutsOut = () => {
 export const getAmoutsIn = () => {
 
 }
+
+export const deposit = async (fromCurrency, toCurrency, amountIn, amountOut, minAmountOut, bptPayload, account, chainId, web3) => {
+    let hash = null
+    const amountInWei = getToWei(amountIn, fromCurrency.decimals).toFixed(0)
+    const minAmountOutWei = getToWei(minAmountOut, toCurrency.decimals).toFixed(0)
+
+    if (amountIn === "" || isZero(amountInWei) || amountOut === "" || isZero(minAmountOutWei)) return { status: false }
+    let sealedContract = getSealedSwapperContract(SEALED_ADDRESS, web3)
+    const swapFunc = swapFuncMaker(fromCurrency, amountInWei, minAmountOutWei, sealedContract, bptPayload)
+    let sendArgs = { from: account }
+    if (fromCurrency.symbol === "ETH") sendArgs["value"] = amountInWei
+    return swapFunc
+        .send(sendArgs)
+        .once('transactionHash', (tx) => {
+            hash = tx
+            SwapTranaction(TransactionState.LOADING, {
+                hash,
+                from: { ...fromCurrency, amount: amountIn },
+                to: { ...toCurrency, amount: amountOut },
+                chainId: chainId,
+            })
+        })
+        .once('receipt', () => SwapTranaction(TransactionState.SUCCESS, {
+            hash,
+            from: { ...fromCurrency, amount: amountIn },
+            to: { ...toCurrency, amount: amountOut },
+            chainId: chainId,
+        }))
+        .once('error', () => SwapTranaction(TransactionState.FAILED, {
+            hash,
+            from: { ...fromCurrency, amount: amountIn },
+            to: { ...toCurrency, amount: amountOut },
+            chainId: chainId,
+        }))
+}
+export const swapFuncMaker = (fromCurrency, amountInWei, minAmountOutWei, sealedContract, balancerMinAmountsOut) => {
+    return () => 1
+}
--- src/helper/useMuon.js
+++ src/helper/useMuon.js
@@ -6,10 +6,9 @@ import useWeb3 from './useWeb3'
 import BigNumber from 'bignumber.js'
 import useRefresh from "./useRefresh";
 import { useERC20 } from './useContract'
-import { getSealedAmountsOut } from './sealedHelper'
-import { isZero, ZERO } from '../constant/number'
+import { getToWei } from "./formatBalance";
 
-export const useSwap = (fromCurrency, toCurrency, amountIn, amountOut, slipage, validChainId = 1, bptPayload) => {
+export const useSwap = (fromCurrency, toCurrency, amountIn, amountOut, slipage, validChainId = 1) => {
     const { account, chainId } = useWeb3React()
     const { fastRefresh } = useRefresh()
     const web3 = useWeb3()
@@ -18,6 +17,7 @@ export const useSwap = (fromCurrency, toCurrency, amountIn, amountOut, slipage, 
     const handleSwap = useCallback(async () => {
         try {
             if (validChainId && chainId !== validChainId) return false
+            return false
             const tx = await swap(
                 fromCurrency,
                 toCurrency,
@@ -39,7 +39,7 @@ export const useSwap = (fromCurrency, toCurrency, amountIn, amountOut, slipage, 
     return { onSwap: handleSwap }
 }
 
-export const useAllocation = (fromCurrency, toCurrency, amountIn, amountOut, slipage, validChainId = 1, bptPayload) => {
+export const useAllocation = (fromCurrency, toCurrency, amountIn, amountOut, slipage, validChainId = 1) => {
     const { account, chainId } = useWeb3React()
     const web3 = useWeb3()
     const { fastRefresh } = useRefresh()
@@ -52,8 +52,6 @@ export const useAllocation = (fromCurrency, toCurrency, amountIn, amountOut, sli
                 toCurrency,
                 amountIn,
                 amountOut,
-                minAmountOut,
-                payload,
                 account,
                 chainId,
                 web3,
@@ -63,7 +61,22 @@ export const useAllocation = (fromCurrency, toCurrency, amountIn, amountOut, sli
             console.log(e);
             return false
         }
-    }, [account, chainId, validChainId, fromCurrency, toCurrency, amountIn, amountOut, minAmountOut, payload, web3, fastRefresh])
+    }, [account, chainId, validChainId, fromCurrency, toCurrency, amountIn, amountOut, web3, fastRefresh])
 
     return { getAllocation: getAllocation }
+}
+
+
+export const useAmountsOut = (from, debouncedAmountIn, type, chainId, price = 20) => {
+    const getAmountsOut = useCallback(() => {
+        return new BigNumber(price).times(debouncedAmountIn).div(0.95).times(1e18)
+    }, [from, debouncedAmountIn, chainId, price, type])
+    return { getAmountsOut }
+}
+
+export const useAmountsIn = (from, debouncedAmountOut, type, chainId, price = 20) => {
+    const getAmountsIn = useCallback(async () => {
+        return getToWei(new BigNumber(0.95).times(debouncedAmountOut).div(price), from.decimals)
+    }, [from, debouncedAmountOut, chainId, price])
+    return { getAmountsIn }
 }

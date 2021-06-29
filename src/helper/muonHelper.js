829d9a43ec37989b59ab19ae1809df7c64331eae
Mohammad Ali <maliabr75@gmail.com>
2021-06-29 21:49:10 +0430

    Add muon node functions

--- src/helper/muonHelper.js
+++ src/helper/muonHelper.js
@@ -1,10 +1,6 @@
-export const deposit = async () => {
+// export const deposit = async () => {
 
-}
-
-export const allocation = async () => {
-    return "100"
-}
+// }
 
 
 export const getAmoutsOut = () => {
@@ -15,7 +11,7 @@ export const getAmoutsIn = () => {
 
 }
 
-export const deposit = async (fromCurrency, toCurrency, amountIn, amountOut, minAmountOut, bptPayload, account, chainId, web3) => {
+/* export const deposit = async (fromCurrency, toCurrency, amountIn, amountOut, minAmountOut, bptPayload, account, chainId, web3) => {
     let hash = null
     const amountInWei = getToWei(amountIn, fromCurrency.decimals).toFixed(0)
     const minAmountOutWei = getToWei(minAmountOut, toCurrency.decimals).toFixed(0)
@@ -48,7 +44,50 @@ export const deposit = async (fromCurrency, toCurrency, amountIn, amountOut, min
             to: { ...toCurrency, amount: amountOut },
             chainId: chainId,
         }))
-}
+} */
 export const swapFuncMaker = (fromCurrency, amountInWei, minAmountOutWei, sealedContract, balancerMinAmountsOut) => {
     return () => 1
 }
+
+//https://app.deus.finance/prices.json
+
+
+export const getSign = async (tokenName, amount, forAddress) => {
+    const baseUrl = "http://104.131.177.195:8080/v1"
+    const payload = `?app=presale&method=deposit&params[token]=${tokenName}&params[amount]=${amount}&params[forAddress]=${forAddress}`
+    return fetcher(baseUrl + payload)
+    // const data = {
+    //     "app": "presale",
+    //     "method": "deposit",
+    //     "params": {
+    //         "token": tokenName,
+    //         "amount": amount,
+    //         "forAddress": forAddress
+    //     }
+    // }
+
+    // return fetcher(baseUrl, {
+    //     method: 'POST',
+    //     body: JSON.stringify(data)
+    // })
+}
+
+export const getAllocation = async () => {
+    return fetcher("https://app.deus.finance/muon-presale.json")
+}
+
+export const getPrices = () => {
+    return fetcher("https://app.deus.finance/prices.json")
+}
+
+
+export const fetcher = async function (url, init) {
+    try {
+        const resp = await fetch(url, init)
+        return await resp.json()
+    } catch (error) {
+        console.log("fetch " + url + " had some error", error);
+    }
+}
+
+

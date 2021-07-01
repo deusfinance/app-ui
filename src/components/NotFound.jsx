7911f1f97e55c7b7591805a31332b894ad12b41b
Mohammad Ali <maliabr75@gmail.com>
2021-07-01 15:48:56 +0430

    Add 404 NotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NotFound extends Component {
     state = {}
     render() {
-        return (<h1>404</h1>);
+        return (<h1 style={{ color: "#ffffff" }}>404 NotFound</h1>);
     }
 }
 

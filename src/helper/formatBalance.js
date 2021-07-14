831cf782edd0a6b72b62b4e2c75243d5ff83668b
Mohammad Ali <maliabr75@gmail.com>
2021-07-14 17:51:57 +0430

    Add RoundNumber

--- src/helper/formatBalance.js
+++ src/helper/formatBalance.js
@@ -1,4 +1,5 @@
 import BigNumber from "bignumber.js";
+import { isZero } from "../constant/number";
 
 export const getBalanceNumber = (balance, decimals = 18) => {
     const displayBalance = new BigNumber(balance).dividedBy(new BigNumber(10).pow(decimals))
@@ -8,7 +9,7 @@ export const getBalanceNumber = (balance, decimals = 18) => {
 
 export const fromWei = (amount, decimals = 18) => {
     const displayBalance = new BigNumber(amount).dividedBy(new BigNumber(10).pow(decimals))
-    return displayBalance.toFixed(decimals, 1)
+    return displayBalance.toFixed(decimals, BigNumber.ROUND_DOWN)
 }
 
 
@@ -20,6 +21,21 @@ export const getToWei = (amount, decimals = 18) => {
 export const getFullDisplayBalance = (balance, decimals = 18) => {
     return new BigNumber(balance).dividedBy(new BigNumber(10).pow(decimals)).toFixed()
 }
+
+export const RoundNumber = (number = null, fixed = 5) => {
+
+    if (!number) return '0'
+    if (isZero(number)) return 0
+
+    BigNumber.config({ EXPONENTIAL_AT: 30 })
+    const bignumber = new BigNumber(number)
+    if (new BigNumber(10).pow(fixed - 1).lte(bignumber)) {
+        return bignumber.toFixed(0, BigNumber.ROUND_DOWN)
+    }
+    return bignumber.toPrecision(fixed, BigNumber.ROUND_DOWN).replace(/\.?0+$/, "")
+}
+
+
 
 export const formatNumber = (number, minPrecision = 2, maxPrecision = 2) => {
     const options = {

fb4f8c29634bd4303da7fdac12be99188086b676
Mohammad Ali <maliabr75@gmail.com>
2021-07-20 13:45:47 +0430

    Add CrossWeb3

--- src/helper/useWeb3.js
+++ src/helper/useWeb3.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react'
 import Web3 from 'web3'
 import { useWeb3React } from '@web3-react/core'
-import { getWeb3NoAccount } from './web3'
+import { getWeb3NoAccount, getWeb3CrossChainNoAccount } from './web3'
 
 const useWeb3 = () => {
     const { library } = useWeb3React()
@@ -10,11 +10,21 @@ const useWeb3 = () => {
 
     useEffect(() => {
         if (library !== refEth.current) {
-            // console.log("callled");
             setweb3(library ? new Web3(library) : getWeb3NoAccount())
             refEth.current = library
         }
     }, [library])
+
+    return web3
+}
+
+export const useCrossWeb3 = (targetChainId) => {
+    const { library, chainId } = useWeb3React()
+    const [web3, setweb3] = useState(library && chainId === targetChainId ? new Web3(library) : getWeb3CrossChainNoAccount(targetChainId))
+
+    useEffect(() => {
+        setweb3(library && chainId === targetChainId ? new Web3(library) : getWeb3CrossChainNoAccount(targetChainId))
+    }, [chainId, library, targetChainId])
 
     return web3
 }

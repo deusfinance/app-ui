aae1038913f0f549e8730b20d0b4dfd6acebe888
Mohammad Ali <maliabr75@gmail.com>
2021-08-09 04:07:36 +0430

    Remove rapid

--- src/context/RefreshContext.js
+++ src/context/RefreshContext.js
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
-const RAPID_INTERVAL = 3000
 const FAST_INTERVAL = 10000
 const SLOW_INTERVAL = 60000
 
-const RefreshContext = React.createContext({ slow: 0, fast: 0, rapid: 0 })
+const RefreshContext = React.createContext({ slow: 0, fast: 0 })
 
-// This context maintain 3 counters that can be used as a dependencies on other hooks to force a periodic refresh
+// This context maintain 2 counters that can be used as a dependencies on other hooks to force a periodic refresh
 const RefreshContextProvider = ({ children }) => {
     const [slow, setSlow] = useState(0)
     const [fast, setFast] = useState(0)
-    const [rapid, setRapid] = useState(0)
-
-    useEffect(() => {
-        const interval = setInterval(async () => {
-            setRapid((prev) => prev + 1)
-        }, RAPID_INTERVAL)
-        return () => clearInterval(interval)
-    }, [])
 
     useEffect(() => {
         const interval = setInterval(async () => {
@@ -33,7 +24,7 @@ const RefreshContextProvider = ({ children }) => {
         return () => clearInterval(interval)
     }, [])
 
-    return <RefreshContext.Provider value={{ slow, fast, rapid }}>{children}</RefreshContext.Provider>
+    return <RefreshContext.Provider value={{ slow, fast }}>{children}</RefreshContext.Provider>
 }
 
 export { RefreshContext, RefreshContextProvider }

965c18c246ddc2eb64eb163b9761690fcf5579cf
Mahdi-HF <mhf1377@gmail.com>
2021-08-10 20:57:43 +0430

    Add NFT page

---
+++ src/pages/NFT/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MainWrapper } from '../../components/App/Swap';
+import { Type } from '../../components/App/Text';
+
+const NFT = () => {
+    return (<>
+        <MainWrapper>
+            <Type.XL fontWeight="300">NFT</Type.XL>
+        </MainWrapper>
+    </>);
+}
+
+export default NFT;

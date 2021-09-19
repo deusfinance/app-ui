2708c3ca7032dd7d38f5ba063e153f84aafc37ed
Mohammad Ali <maliabr75@gmail.com>
2021-09-19 10:26:49 +0430

    Update migrator

--- src/pages/Migrator/index.js
+++ src/pages/Migrator/index.js
@@ -12,13 +12,34 @@ export const MainWrapper = styled.div`
 export const Container = styled.div`
    margin-top: 60px;
    background: linear-gradient(180deg, #18191D 0%, #18191D 100%);
-   border: 1px solid #000000;
+   /* border: 1px solid #000000; */
    border-radius: 15px 15px 0px 0px;
 `
 
 export const Title = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 12px;
    height: 70px;
    border: 1px solid #000000;
+`
+export const TokensContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 22px;
+   height: 70px;
+   border: 1px solid #000000;
+`
+
+export const Token = styled.div`
+    background: #272727;
+    border: 1px solid #000000;
+    box-sizing: border-box;
+    border-radius: 15px;
+    width: 160px;
+    height: 100px;
 `
 
 
@@ -28,8 +49,16 @@ const Migrator = () => {
         <Type.XXL fontWeight="300">Migrator</Type.XXL>
         <Container>
             <Title>
-                {"->"}
+                <div style={{ display: 'flex', justifyContent: "flex-start", alignItems: "center" }}>
+                    <svg style={{ margin: "0 10px" }} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="20" height="20" rx="3" fill="#CECECE" />
+                    </svg>
+                    <Type.LG fontWeight="300">Migrate: </Type.LG>
+                    <Type.LG style={{ marginLeft: "5px" }} fontWeight="300"> DEA / sDEA </Type.LG>
+                </div>
+                <div>&#8594;</div>
             </Title>
+
         </Container>
     </MainWrapper>);
 }

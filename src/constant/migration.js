2d5b1335dde2d60ca53e573a6d8f96259cb16436
Mohammad Ali <maliabr75@gmail.com>
2021-09-23 14:07:26 +0330

    Change text

--- src/constant/migration.js
+++ src/constant/migration.js
@@ -1,109 +1,3 @@
-// export const MIGRATION_CONFIG = [
-//     {
-//         id: 0,
-//         title: {
-//             from: "DEA / sDEA",
-//             to: "DEUS V2"
-//         },
-//         tokens: {
-//             from: [
-//                 { symbol: "DEA", balance: "342.23" },
-//                 { symbol: "sDEA", balance: "342.23" },
-//             ],
-//             to: [
-//                 { symbol: "DEUS", amount: "342.23" },
-//             ]
-//         }
-//     },
-//     {
-//         id: 1,
-//         title: {
-//             from: "DEUS / sDEUS",
-//             to: "DEUS V2"
-//         },
-//         tokens: {
-//             from: [
-//                 { symbol: "DEUS", balance: "0" },
-//                 { symbol: "sDEUS", balance: "342.23" },
-//             ],
-//             to: [
-//                 { symbol: "DEUS/DEI LP", amount: "100" },
-//                 { symbol: "DEUS", amount: "100" },
-//             ]
-//         }
-//     },
-//     {
-//         id: 2,
-//         title: {
-//             from: "sUNI , BPT",
-//             to: "DEUS V2"
-//         },
-//         tokens: {
-//             from: [
-//                 { symbol: "sUNI-DD", balance: "1.23587" },
-//                 { symbol: "sUNI-DE", balance: "1.23587" },
-//                 { symbol: "sUNI-DU", balance: "1.23587" },
-//                 { symbol: "BPT", balance: "1.23587" },
-//             ],
-//             to: [
-//                 { symbol: "DEUS/DEI LP", amount: "100" },
-//                 { symbol: "DEUS", amount: "100" },
-//             ]
-//         }
-//     },
-//     {
-//         id: 3,
-//         title: {
-//             from: "Uni DEUS/DEA LP",
-//             to: "DEUS V2"
-//         },
-//         tokens: {
-//             from: [
-//                 { symbol: "Uni-DEUS/DEA", balance: "1.23587" },
-//             ],
-//             to: [
-//                 { symbol: "DEUS/DEI LP", amount: "100" },
-//                 { symbol: "DEUS", amount: "100" },
-//             ]
-//         }
-//     },
-//     {
-//         id: 4,
-//         title: {
-//             from: "Uni DEA/USDC LP",
-//             to: "DEUS V2"
-//         },
-//         tokens: {
-//             from: [
-//                 { symbol: "Uni-DEA/USDC", balance: "1.23587" },
-//             ],
-//             to: [
-//                 { symbol: "DEUS/DEI LP", amount: "100" },
-//                 { symbol: "DEUS", amount: "100" },
-//                 { symbol: "DEUS/USDC LP", amount: "100" },
-//             ]
-//         }
-//     },
-//     {
-//         id: 5,
-//         title: {
-//             from: "Uni DEUS/ETH LP",
-//             to: "DEUS V2"
-//         },
-//         tokens: {
-//             from: [
-//                 { symbol: "Uni-DEUS/ETH", balance: "1.23587" },
-//             ],
-//             to: [
-//                 { symbol: "DEUS/DEI LP", amount: "100" },
-//                 { symbol: "DEUS", amount: "100" },
-//             ]
-//         }
-//     },
-// ]
-
-
-
 export const snapShotMaker = (snap) => {
     const {
         BPT_balance,
@@ -121,7 +15,7 @@ export const snapShotMaker = (snap) => {
         {
             id: 0,
             title: {
-                from: "DEA / sDEA",
+                from: "DEA , sDEA",
                 to: "DEUS V2"
             },
             tokens: {
@@ -137,7 +31,7 @@ export const snapShotMaker = (snap) => {
         {
             id: 1,
             title: {
-                from: "DEUS / sDEUS",
+                from: "DEUS , sDEUS",
                 to: "DEUS V2"
             },
             tokens: {

8a084a8c2f8e2d0634dde5874074e575cac33b04
Mohammad Ali <maliabr75@gmail.com>
2021-10-21 16:46:18 +0330

    Change quickswap link

--- src/components/App/Dei/BuyDEUS.jsx
+++ src/components/App/Dei/BuyDEUS.jsx
@@ -62,7 +62,7 @@ const DeiTokenBox = () => {
                     <ExternalLink textDecoration="none" href={`https://app.uniswap.org/#/swap?use=V2&outputCurrency=${deusToken.address}&inputCurrency=ETH`}> Buy  <img src="/tokens/deus.svg" style={{ width: "20px", marginLeft: "4px", marginRight: "2px" }} alt="deus" />DEUS on  Uniswap <img src="/img/uniswap.png" style={{ width: "20px", marginLeft: "4px" }} alt="uniswap" /></ExternalLink>
                 </EachUrl>
                 <EachUrl className="quick">
-                    <ExternalLink textDecoration="none" href={`https://quickswap.exchange/#/swap?outputCurrency=${deusToken.address}&inputCurrency=0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270`}> Buy <img src="/tokens/deus.svg" style={{ width: "20px", marginLeft: "4px", marginRight: "2px", }} alt="deus" />DEUS on QuickSwap <img src="/img/quickswap.png" style={{ width: "20px", marginLeft: "4px", borderRadius: "50%" }} alt="quickswap" /></ExternalLink>
+                    <ExternalLink textDecoration="none" href={`https://quickswap.exchange/#/swap?outputCurrency=${deusToken.address}&inputCurrency=MATIC`}> Buy <img src="/tokens/deus.svg" style={{ width: "20px", marginLeft: "4px", marginRight: "2px", }} alt="deus" />DEUS on QuickSwap <img src="/img/quickswap.png" style={{ width: "20px", marginLeft: "4px", borderRadius: "50%" }} alt="quickswap" /></ExternalLink>
                 </EachUrl>
 
             </MainWrapper>

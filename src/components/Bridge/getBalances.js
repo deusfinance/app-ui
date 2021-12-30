8713101b2ae44c3dab5609a0a98f2d1d87f62dd7
Mahdi-HF <mhf1377@gmail.com>
2021-12-30 20:37:21 +0330

    feat(contract): Add bug to fix later

--- src/components/Bridge/getBalances.js
+++ src/components/Bridge/getBalances.js
@@ -11,12 +11,16 @@ import { ChainId } from '../../constant/web3'
 const useTokenBalances = (chains, tokens, fetchData) => {
   const [balances, setBalances] = useState(tokens)
   const { account, chainId } = useWeb3React()
+  
   const ethWeb3 = useCrossWeb3(ChainId.ETH)
   const bscWeb3 = useCrossWeb3(ChainId.BSC)
   // const rinkebyWeb3 = useCrossWeb3(ChainId.RINKEBY)
   // const bscTestWeb3 = useCrossWeb3(ChainId.BSC_TESTNET)
   const ftmWeb3 = useCrossWeb3(ChainId.FTM)
   const polygonWeb3 = useCrossWeb3(ChainId.MATIC)
+  const metisWeb3 = useCrossWeb3(ChainId.METIS)
+  const arbitrumWeb3 = useCrossWeb3(ChainId.ARBITRUM)
+  // const optimisticWeb3 = useCrossWeb3(ChainId.OPTIMISTIC)
   const web3s = {
     [ChainId.ETH]: ethWeb3,
     [ChainId.FTM]: ftmWeb3,
@@ -24,6 +28,9 @@ const useTokenBalances = (chains, tokens, fetchData) => {
     [ChainId.BSC]: bscWeb3,
     // [ChainId.RINKEBY]: rinkebyWeb3,
     // [ChainId.BSC_TESTNET]: bscTestWeb3,
+    [ChainId.METIS]: metisWeb3,
+    [ChainId.ARBITRUM]: arbitrumWeb3,
+    // [ChainId.OPTIMISTIC]: optimisticWeb3,
   }
   useEffect(() => {
     const fetchBalances = async () => {
